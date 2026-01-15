68f86cf09c1c06820144864df343cb8040d296f2
Rasmus Christensen <raschr@arkivverket.no>
2026-01-15 19:33:34 +0100

    Fix: correct path and casing in startup script

--- app_start.sh
+++ app_start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Go to the deployed directory (important!)
-cd /home/site/wwwroot
-
-# Install dependencies manually
-pip install -r backend/requirements.txt
-
-# Start FastAPI with uvicorn
-Cd backend
+cd /home/site/wwwroot/backend
+pip install -r requirements.txt
 python -m uvicorn main:app --host=0.0.0.0 --port=8000
