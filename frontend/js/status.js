8915c466d0a1dc455e9a843a4f392117eacbc942
Rasmus Christensen <rasmus.christensen@hotmail.com>
2026-03-25 13:36:53 +0100

    Modified timestamp in status page

--- frontend/js/status.js
+++ frontend/js/status.js
@@ -51,7 +51,15 @@ function renderStatus(rows) {
     tr.innerHTML = `
       <td><span class="status-dot ${color}"></span></td>
       <td>${row.TableName}</td>
-      <td>${lastLoaded.toLocaleString("no-NO")}</td>
+      <td>${lastLoaded.toLocaleString("no-NO", {
+        timeZone: "Europe/Oslo",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+      })}</td>
     `;
 
     tableBody.appendChild(tr);
